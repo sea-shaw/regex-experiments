cf091c2b38db320f6e6759317c38409b51b2fccf
Charlie Shaw <charliecpshaw@gmail.com>
2026-06-28 17:17:58 +0100

    feat: nicer types

--- src/main/scala/regex.scala
+++ src/main/scala/regex.scala
@@ -1,29 +1,56 @@
 package experiments
 
 import cats.collections.Diet
+import scala.Tuple.Concat
 
 object regex {
 
-  sealed trait Regex[A <: Tuple] {
+  type ToTuple[A] <: Tuple = A match {
+    case EmptyTuple => EmptyTuple
+    case a *: as    => a *: as
+    case Unit       => EmptyTuple
+    case _          => Tuple1[A]
+  }
+
+  type FromTuple[A] = A match {
+    case EmptyTuple => Unit
+    case Tuple1[a] => a
+    case _         => A
+  }
+
+  type OptCapture[A] = A match {
+    case Unit => Unit
+    case _    => Option[A]
+  }
+
+  type AltCapture[A, B] = (A, B) match {
+    case (Unit, Unit) => Unit
+    case (a, b)       => Either[a, b]
+  }
+
+  sealed trait Regex[A] {
     def get: A = ???
     def unapply(s: String): Option[A] = ???
   }
 
-  case class Lit(c: Int) extends Regex[EmptyTuple]
+  case class Lit(c: Int) extends Regex[Unit]
 
-  case class Class(cs: Diet[Int]) extends Regex[EmptyTuple]
+  case class Class(cs: Diet[Int]) extends Regex[Unit]
 
-  case object Dot extends Regex[EmptyTuple]
+  case object Dot extends Regex[Unit]
 
-  case class Opt[A <: Tuple](r: Regex[A]) extends Regex[Tuple1[Option[A]]]
-  
-  case class Rep0[A <: Tuple](r: Regex[A]) extends Regex[Tuple1[Option[A]]]
+  case class Opt[A](r: Regex[A]) extends Regex[OptCapture[FromTuple[A]]]
 
-  case class Rep1[A <: Tuple](r: Regex[A]) extends Regex[A]
+  case class Cat[A , B](r1: Regex[A], r2: Regex[B]) extends Regex[FromTuple[Concat[ToTuple[A], ToTuple[B]]]]
 
-  case class Cat[A <: Tuple, B <: Tuple](r1: Regex[A], r2: Regex[B]) extends Regex[Tuple.Concat[A, B]]
+  case class Alt[A , B](r1: Regex[A], r2: Regex[B]) extends Regex[AltCapture[FromTuple[A], FromTuple[B]]]
 
-  case class Alt[A <: Tuple, B <: Tuple](r1: Regex[A], r2: Regex[B]) extends Regex[Tuple1[Either[A, B]]]
+  case class Capture[A](r: Regex[A]) extends Regex[FromTuple[String *: ToTuple[A]]]
 
-  case class Capture[A <: Tuple](r: Regex[A]) extends Regex[String *: A]
+  private val tests: Unit = {
+    val cap = Capture(Dot)
+    val y = cap.get
+    val regex = Cat(Cat(Alt(cap, cap), Alt(Dot, Dot)), Cat(Opt(cap), Opt(Dot)))
+    val x = regex.get
+  }
 }
