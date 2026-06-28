ebf5e2f530c8a423dcdf5449094c1fa2252f3365
Charlie Shaw <charliecpshaw@gmail.com>
2026-06-28 23:37:45 +0100

    feat: dependent types

--- src/main/scala/regex.scala
+++ src/main/scala/regex.scala
@@ -23,37 +23,80 @@ object regex {
     case _    => Option[A]
   }
 
-  type AltCapture[A, B] = (A, B) match {
-    case (Unit, Unit) => Unit
-    case (a, b)       => Either[a, b]
+  type AltCapture[A, B] = A match {
+    case Unit => B match {
+      case Unit => Unit
+    }
+    case _ => Either[A, B]
   }
 
   type CatCapture[A, B] = FromTuple[Concat[ToTuple[A], ToTuple[B]]]
 
   type CaptureType[A] = FromTuple[String *: ToTuple[A]]
 
-  sealed trait Regex[+A] {
-    def unapply(s: String): Option[A] = ???
+  private def toTuple[A](x: A): ToTuple[A] = x match {
+    case _: EmptyTuple => EmptyTuple
+    case x: (_ *: _)   => x
+    case _: Unit       => EmptyTuple
+    case x: Any        => x *: EmptyTuple
   }
 
-  case class Lit(c: Int) extends Regex[Unit]
+  private def fromTuple[A <: Tuple](x: A): FromTuple[A] = x match {
+    case _: EmptyTuple => ()
+    case x: Tuple1[_]  => x._1
+    case x: Any        => x
+  }
 
-  case class Class(cs: Diet[Int]) extends Regex[Unit]
+  sealed trait Regex[+A] {
+    def unapply(s: String): Option[A] = ???
+    def shape: A
+  }
 
-  case object Dot extends Regex[Unit]
+  case class Lit(c: Int) extends Regex[Unit] {
+    override def shape: Unit = ()
+  }
 
-  case class Opt[A](r: Regex[A]) extends Regex[OptCapture[A]]
+  case class Class(cs: Diet[Int]) extends Regex[Unit] {
+    override def shape: Unit = ()
 
-  case class Alt[A , B](r1: Regex[A], r2: Regex[B]) extends Regex[AltCapture[A, B]]
+  }
 
-  case class Cat[A , B](r1: Regex[A], r2: Regex[B]) extends Regex[CatCapture[A, B]]
+  case object Dot extends Regex[Unit] {
+    override def shape: Unit = ()
 
-  case class Capture[A](r: Regex[A]) extends Regex[CaptureType[A]]
+  }
+
+  case class Opt[A](r: Regex[A]) extends Regex[OptCapture[A]] {
+    override def shape: OptCapture[A] = r.shape match {
+      case _: Unit => ()
+      case x: Any  => Some(x)
+    }
+  }
+
+  case class Alt[A, B](r1: Regex[A], r2: Regex[B]) extends Regex[AltCapture[A, B]] {
+    override def shape: AltCapture[A, B] = r1.shape match {
+      case _: Unit => r2.shape match {
+        case _: Unit => ()
+      }
+      case x: Any => Right(r2.shape)
+    }
+  }
+
+  case class Cat[A, B](r1: Regex[A], r2: Regex[B]) extends Regex[CatCapture[A, B]] {
+    override def shape: CatCapture[A, B] = fromTuple(toTuple(r1.shape) ++ toTuple(r2.shape))
+  }
+
+  case class Capture[A](r: Regex[A]) extends Regex[CaptureType[A]] {
+    override def shape: CaptureType[A] = {
+      fromTuple(("" *: toTuple(r.shape)))
+    }
+  }
 
   private val tests: Unit = {
     val cap = Capture(Dot)
     val y = cap.unapply(???).get
     val regex = Cat(Cat(Alt(cap, cap), Alt(Dot, Dot)), Cat(Opt(cap), Opt(Dot)))
     val x = regex.unapply(???).get
+    val s = regex.shape
   }
 }
