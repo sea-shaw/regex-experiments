6c7b6fda996ed46637fbac38a55e2f6453e56ec3
Charlie Shaw <charliecpshaw@gmail.com>
2026-06-29 16:04:30 +0100

    feat (match-types): handle nested groups

--- src/main/scala/match-types.scala
+++ src/main/scala/match-types.scala
@@ -6,6 +6,7 @@ import scala.compiletime.ops.string.{CharAt, Length}
 import scala.Tuple.Reverse
 
 object matchtypes {
+
   type Captures[S <: String & Singleton] = Go[S, 0, Length[S], EmptyTuple] match {
     case (a, _) => a
   }
@@ -13,8 +14,11 @@ object matchtypes {
   type Go[S <: String & Singleton, L <: Int, U <: Int, Acc <: Tuple] <: (Any, Int) = L match {
     case U => (Tidy[Reverse[Acc]], U)
     case _ => CharAt[S, L] match {
-      case '('  => Go[S, L + 1, U, String *: Acc]
       case '\\' => Go[S, L + 2, U, Acc]
+      case '('  => Go[S, L + 1, Nothing, EmptyTuple] match {
+        case (a, l) => Go[S, l, U, a *: Acc]
+      }
+      case ')'  => (Tidy[String *: Reverse[Acc]], L + 1)
       case _    => Go[S, L + 1, U, Acc]
     }
   }
@@ -29,6 +33,7 @@ object matchtypes {
     val zero: Captures["a"] = ()
     val one: Captures["(a)"] = "a"
     val two: Captures["(a)(b)"] = ("a", "b")
-    val escape: Captures["\\(a\\)"] = () 
+    val escape: Captures["\\(a\\)"] = ()
+    val nested: Captures["(a(b)(c))(d)"] = (("a", "b", "c"), "d")
   }
 }
