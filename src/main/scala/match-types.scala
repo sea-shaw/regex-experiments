ffd6e3c6bcbba8e1d697ec7d66a50ef4a9954d1a
Charlie Shaw <charliecpshaw@gmail.com>
2026-06-29 17:18:24 +0100

    feat (match-types): add non-capturing groups

--- src/main/scala/match-types.scala
+++ src/main/scala/match-types.scala
@@ -6,28 +6,29 @@ import scala.Tuple.{Concat, Reverse}
 
 object matchtypes {
 
-  type Captures[S <: String & Singleton] = Go[S, 0, Length[S], EmptyTuple] match {
+  type Captures[S <: String & Singleton] = Go[S, 0, Length[S], false, EmptyTuple] match {
     case (a, _) => a
   }
 
-  type Go[S <: String & Singleton, L <: Int, U <: Int, Acc <: Tuple] <: (Any, Int) = L match {
+  type Go[S <: String, L <: Int, U <: Int, Cap <: Boolean, Acc <: Tuple] <: (Any, Int) = L match {
     case U => (Tidy[Reverse[Acc]], U)
     case _ => CharAt[S, L] match {
-      case '\\' => Go[S, L + 2, U, Acc]
-      case '('  => Go[S, L + 1, U, EmptyTuple] match {
+      case '\\' => Go[S, L + 2, Cap, U, Acc]
+      case '('  => Go[S, L + 1, U, IsCapturing[S, L + 1], EmptyTuple] match {
         case (a, l) => a match {
-          case Tuple => Go[S, l, U, Concat[a, Acc]]
-          case _     => Go[S, l, U, a *: Acc]
+          case Tuple => Go[S, l, U, Cap, Concat[a, Acc]]
+          case Unit  => Go[S, l, U, Cap, Acc]
+          case _     => Go[S, l, U, Cap, a *: Acc]
         }
       }
       case ')'  => L + 1 match {
-        case U => (Tidy[String *: Reverse[Acc]], U)
+        case U => (CloseGroup[Cap, Acc], U)
         case _ => CharAt[S, L + 1] match {
-          case '?' | '*' => (Option[Tidy[String *: Reverse[Acc]]], L + 2)
-          case _         => (Tidy[String *: Reverse[Acc]], L + 1)
+          case '?' | '*' => (Option[CloseGroup[Cap, Acc]], L + 2)
+          case _         => (CloseGroup[Cap, Acc], L + 1)
         }
       }
-      case _    => Go[S, L + 1, U, Acc]
+      case _    => Go[S, L + 1, U, Cap, Acc]
     }
   }
 
@@ -35,6 +36,22 @@ object matchtypes {
     case EmptyTuple => Unit
     case Tuple1[a]  => a
     case _          => T
+  }
+
+  type IsCapturing[S <: String, I <: Int] <: Boolean = CharAt[S, I] match {
+    case '?' => CharAt[S, I + 1] match {
+      case '<' => CharAt[S, I + 2] match {
+        case '=' | '!' => false
+        case _         => true
+      }
+      case _ => false
+    }
+    case _ => true
+  }
+
+  type CloseGroup[Cap <: Boolean, Acc <: Tuple] = Cap match {
+    case true  => Tidy[String *: Reverse[Acc]]
+    case false => Tidy[Reverse[Acc]]
   }
 
   private val tests: Unit = {
@@ -50,12 +67,16 @@ object matchtypes {
 
     val optional: Captures["(a)?"] = Some("a")
     val catOptional: Captures["(a)?(b)?"] = (Some("a"), Some("b"))
-    val catInOptional: Captures["(a(b))?"] = Some(("a", "b"))
+    val catInOptional: Captures["(a(b))?"] = Some(("ab", "b"))
     val nestedOptional: Captures["(a(b)?)?"] = Some(("ab", Some("b")))
 
     val star: Captures["(a)*"] = Some("a")
 
+    val nonCap: Captures["(?:a)"] = ()
+    val capsInNonCap: Captures["(?:(a)(b))"] = ("a", "b")
+    val capsInOptNonCap: Captures["(?:(a)(b))?"] = Some(("a", "b"))
+
     // TODO: Keep track of level of brackets
-    val bug: Captures["(a))"] = ("a", "???")
+    val bug: Captures["(a))"] = "a"
   }
 }
