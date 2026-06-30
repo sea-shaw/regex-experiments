0009cb188e998736110299aa6424137a94567cfc
Charlie Shaw <charliecpshaw@gmail.com>
2026-06-30 17:59:42 +0100

    feat (dependent typing): regex class

--- src/main/scala/matchtypes/dependenttyping.scala
+++ src/main/scala/matchtypes/dependenttyping.scala
@@ -3,18 +3,36 @@ package experiments.matchtypes
 import experiments.matchtypes.matchtypes.{Captures, Fst, Go, Group, IsCapturing, Tidy}
 import scala.compiletime.ops.int.+
 import scala.compiletime.ops.string.{CharAt, Length}
+import java.util.regex.Pattern
 
 object dependenttyping {
 
-  def sanitiser[R <: String & Singleton](r: R): Array[String | Null] => Option[Captures[R]] = {
-    go(r, 0, false, EmptyTuple).andThen(_.map(fst))
+  class Regex[+A] private(regex: String, sanitise: Array[String | Null] => Option[A]) {
+    private val pattern: Pattern = Pattern.compile(regex)
+    def unapply(s: String): Option[A] = {
+      val m = pattern.matcher(s)
+      if (m.matches()) {
+        val a = Array.tabulate(m.groupCount)(i => m.group(i + 1))
+        sanitise(a)
+      } else {
+        None
+      }
+    }
+  }
+
+  object Regex {
+    def apply[R <: String & Singleton](r: R): Regex[Captures[R]] = new Regex(r, sanitise(r))
+  }
+
+  def sanitise[R <: String & Singleton](r: R)(groups: Array[String | Null]): Option[Captures[R]] = {
+    go(r, 0, false, EmptyTuple)(groups, 0)._1.map(fst)
   }
 
   private def fst[T <: Tuple3[?, ?, ?]](t: T): Fst[T] = t match {
     case t: (_, _, _) => t._1
   }
 
-  private def go[R <: String, I <: Int & Singleton, Cap <: Boolean & Singleton, Acc <: Tuple](r: R, i: I, cap: Cap, acc: Acc): Array[String | Null] => Option[Go[R, I, Cap, Acc]] = ???
+  private def go[R <: String, I <: Int & Singleton, Cap <: Boolean & Singleton, Acc <: Tuple](r: R, i: I, cap: Cap, acc: Acc)(groups: Array[String | Null], groupNo: Int): (Option[Go[R, I, Cap, Acc]], Int, Boolean) = ???
 
   private def tidy[T <: Tuple](t: T): Tidy[T] = t match {
     case _: EmptyTuple => ()
