b1463f1e30e34e1c3d2b4535886b24af26dd031c
Charlie Shaw <charliecpshaw@gmail.com>
2026-07-03 22:38:29 +0100

    feat (ast): sanitise macros for each case

--- src/main/scala/macros/ast.scala
+++ src/main/scala/macros/ast.scala
@@ -1,6 +1,7 @@
 package experiments.macros
 
-import experiments.macros.hlist.{Concat, HCons, HList, HNil}
+import cats.syntax.all.catsSyntaxTuple2Semigroupal
+import experiments.macros.hlist.{Concat, HCons, HList, HNil, ++}
 import scala.quoted.{Expr, Quotes, Type}
 
 object ast {
@@ -8,12 +9,36 @@ object ast {
     def sanitiseCode(groups: Expr[Array[String | Null]], i: Int)(using Quotes, Type[A]): (Expr[(Option[A], Boolean)], Int)
   }
 
-  case class Capture[A <: HList: Type](r: Regex[A]) extends Regex[HCons[String, A]] {
-    override def sanitiseCode(groups: Expr[Array[String | Null]], i: Int)(using Quotes, Type[HCons[String, A]]): (Expr[(Option[HCons[String, A]], Boolean)], Int) = ???
+  case class Capture[A <: HList: Type](inner: Regex[A]) extends Regex[HCons[String, A]] {
+    override def sanitiseCode(groups: Expr[Array[String | Null]], i: Int)(using Quotes, Type[HCons[String, A]]): (Expr[(Option[HCons[String, A]], Boolean)], Int) = {
+      val idx = Expr(i)
+      val (sanitisedInner, j) = inner.sanitiseCode(groups, i + 1)
+      val sanitised = '{
+        val outerCap = Option($groups($idx))
+        val cap = outerCap.flatMap { s =>
+          val (innerCap, _) = $sanitisedInner
+          innerCap.map(HCons(s, _))
+        }
+        (cap, outerCap.isDefined)
+      }
+      (sanitised, j)
+    }
   }
 
   case class Alt[A <: HList: Type, B <: HList: Type](left: Regex[A], right: Regex[B]) extends Regex[HCons[Either[A, B], HNil]] {
-    override def sanitiseCode(groups: Expr[Array[String | Null]], i: Int)(using Quotes, Type[HCons[Either[A, B], HNil]]): (Expr[(Option[HCons[Either[A, B], HNil.type]], Boolean)], Int) = ???
+    override def sanitiseCode(groups: Expr[Array[String | Null]], i: Int)(using Quotes, Type[HCons[Either[A, B], HNil]]): (Expr[(Option[HCons[Either[A, B], HNil.type]], Boolean)], Int) = {
+      val (sanitisedLeft, j) = left.sanitiseCode(groups, i)
+      val (sanitisedRight, k) = right.sanitiseCode(groups, j)
+      val sanitised = '{
+        val (leftCaps, anyLeft) = $sanitisedLeft
+        val (rightCaps, anyRight) = $sanitisedRight
+        val left = leftCaps.map(Left(_))
+        val right = rightCaps.map(Right(_))
+        val caps = if anyLeft then left.orElse(right) else right.orElse(left)
+        (caps.map(HCons(_, HNil)), anyLeft || anyRight)
+      }
+      (sanitised, k)
+    }
   }
 
   case class Opt[A <: HList: Type](r: Regex[A]) extends Regex[HCons[Option[A], HNil]] {
@@ -26,9 +51,18 @@ object ast {
       (sanitised, j)
     }
   }
-  
+
   case class Cat[A <: HList: Type, B <: HList: Type](left: Regex[A], right: Regex[B]) extends Regex[Concat[A, B]] {
-    override def sanitiseCode(groups: Expr[Array[String | Null]], i: Int)(using Quotes, Type[Concat[A, B]]): (Expr[(Option[Concat[A, B]], Boolean)], Int) = ???
+    override def sanitiseCode(groups: Expr[Array[String | Null]], i: Int)(using Quotes, Type[Concat[A, B]]): (Expr[(Option[Concat[A, B]], Boolean)], Int) = {
+      val (sanitisedLeft, j) = left.sanitiseCode(groups, i)
+      val (sanitisedRight, k) = right.sanitiseCode(groups, j)
+      val sanitised = '{
+        val (leftCaps, anyLeft) = $sanitisedLeft
+        val (rightCaps, anyRight) = $sanitisedRight
+        ((leftCaps, rightCaps).mapN(_ ++ _), anyLeft || anyRight)
+      }
+      (sanitised, k)
+    }
   }
 
   case class Lit(s: String) extends Regex[HNil] {
