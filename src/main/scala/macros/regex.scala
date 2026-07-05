1532913a2575c853af51d69914135d7feae24918
Charlie Shaw <charliecpshaw@gmail.com>
2026-07-05 21:46:54 +0100

    feat (regex): transparent regex macro

---
+++ src/main/scala/macros/regex.scala
@@ -0,0 +1,56 @@
+package experiments.macros
+
+import experiments.macros.ast.{Regex => RegexAST}
+import experiments.macros.hlist.HList
+import experiments.macros.parser.parse
+import scala.quoted.{Expr, Quotes, quotes}
+import scala.quoted.Type
+import java.util.regex.Pattern
+
+object regex {
+  sealed trait Regex[A] {
+    def unapply(s: String): Option[A]
+  }
+
+  object Regex {
+    transparent inline def apply(inline s: String): Regex[?] = ${ isInlineable('s) }
+
+    private def isInlineable(s: Expr[String])(using Quotes): Expr[Regex[?]] = {
+      import quotes.reflect.report
+      s match {
+        case Expr(s) => parse(s) match {
+          case Right(ast) => regexCode(s, ast)
+          case Left(err)  => report.errorAndAbort(err)
+        }
+        case _       => report.errorAndAbort("Regex string must be compile-time constant.")
+      }
+    }
+
+    private def regexCode[A <: HList](regexStr: String, ast: RegexAST[A])(using Quotes): Expr[Regex[A]] = {
+      import quotes.reflect.{Position, report}
+
+      given Type[A] = ast.getType
+
+      val regexStrExpr = Expr(regexStr)
+      val expr = '{
+        new Regex[A] {
+          private val pattern: Pattern = Pattern.compile($regexStrExpr)
+
+          override def unapply(s: String): Option[A] = {
+            val m = pattern.matcher(s)
+            if (m.matches()) {
+              val groups = Array.tabulate(m.groupCount)(i => Option(m.group(i + 1)))
+              val (sanitised, _) = ${ ast.sanitiseCode('groups, 0)._1 }
+              sanitised
+            } else {
+              None
+            }
+          }
+        }
+      }
+
+      report.info(expr.show, Position.ofMacroExpansion)
+      expr
+    }
+  }
+}
