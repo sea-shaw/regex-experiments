c8e4393ce71d96fd1bcb8bef929dfd87658e732f
Charlie Shaw <charliecpshaw@gmail.com>
2026-07-21 16:18:56 +0100

    refactor (tidy): use one accumulator list of types

--- src/main/scala/macros/tidy.scala
+++ src/main/scala/macros/tidy.scala
@@ -24,20 +24,17 @@ object tidy {
 
   private def tidyType[A <: HChain: Type](using Quotes): Type[?] = {
     @tailrec
-    def build[Acc <: Tuple: Type](tpe: Type[? <: HChain], types: List[Type[? <: HChain]]): Type[? <: Tuple] = tpe match {
-      case '[HEmpty] => types match {
-        case tpe :: types => build[Acc](tpe, types)
-        case Nil          => Type.of[Acc]
-      }
-      case '[HSingleton[a]] => types match {
-        case tpe :: types => build[a *: Acc](tpe, types)
-        case Nil          => Type.of[a *: Acc]
-      }
-      case '[type a <: HChain; type b <: HChain; HAppend[a, b]] => {
-        build[Acc](Type.of[b], Type.of[a] :: types)
+    def build[Acc <: Tuple: Type](types: List[Type[? <: HChain]]): Type[? <: Tuple] = types match {
+      case Nil => Type.of[Acc]
+      case tpe :: types => tpe match {
+        case '[HEmpty]        => build[Acc](types)
+        case '[HSingleton[a]] => build[a *: Acc](types)
+        case '[type a <: HChain; type b <: HChain; HAppend[a, b]] => {
+          build[Acc](Type.of[b] :: Type.of[a] :: types)
+        }
       }
     }
 
-    build[EmptyTuple](Type.of[A], Nil)
+    build[EmptyTuple](Type.of[A] :: Nil)
   }
 }
