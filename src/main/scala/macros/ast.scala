dd39cbbc18bef9206e85d420c50acb01b3ea0df5
Charlie Shaw <charliecpshaw@gmail.com>
2026-07-21 12:31:42 +0100

    feat (ast): implement monad for sanitised

--- src/main/scala/macros/ast.scala
+++ src/main/scala/macros/ast.scala
@@ -2,13 +2,14 @@ package experiments.macros
 
 import experiments.macros.evidence.{apply, liftCo}
 import experiments.macros.hcollections.hchain.{HChain, HConcat, HCons, HEmpty, HSingleton, Tidy}
-import cats.{Applicative, Eval, Functor, Traverse}
+import cats.{Applicative, Eval, Functor, Monad, Traverse}
 import cats.collections.Diet
 import cats.data.{Ior, State}
 import cats.kernel.Order
 import cats.syntax.all.*
 import scala.quoted.{Expr, Quotes, Type}
 import parsley.templates.PureParserBridge0
+import scala.annotation.tailrec
 
 object ast {
 
@@ -34,6 +35,27 @@ object ast {
 
       override def ap[A, B](ff: Sanitised[A => B])(fa: Sanitised[A]): Sanitised[B] = {
         Sanitised(ff.captures(fa.captures), ff.any || fa.any)
+      }
+    }
+
+    given Monad[Sanitised] {
+      override def pure[A](x: A): Sanitised[A] = {
+        Sanitised(x, false)
+      }
+
+      override def flatMap[A, B](fa: Sanitised[A])(f: A => Sanitised[B]): Sanitised[B] = {
+        val fb = f(fa.captures)
+        Sanitised(fb.captures, fa.any || fb.any)
+      }
+
+      override def tailRecM[A, B](a: A)(f: A => Sanitised[Either[A, B]]): Sanitised[B] = {
+        @tailrec
+        def go(a: A, any: Boolean): Sanitised[B] = f(a) match {
+          case Sanitised(Left(value), leftAny) => go(value, any || leftAny)
+          case Sanitised(Right(value), rightAny) => Sanitised(value, any || rightAny)
+        }
+
+        go(a, false)
       }
     }
 
@@ -70,6 +92,20 @@ object ast {
 
       override def ap[A, B](ff: SanitisedT[F, A => B])(fa: SanitisedT[F, A]): SanitisedT[F, B] = {
         SanitisedT((ff.value, fa.value).mapN(_ ap _))
+      }
+    }
+
+    given [F[_]: Monad] => Monad[[A] =>> SanitisedT[F, A]] {
+      override def pure[A](x: A): SanitisedT[F, A] = {
+        SanitisedT(Monad[F].pure(Monad[Sanitised].pure(x)))
+      }
+
+      override def flatMap[A, B](fa: SanitisedT[F, A])(f: A => SanitisedT[F, B]): SanitisedT[F, B] = {
+        ???
+      }
+
+      override def tailRecM[A, B](a: A)(f: A => SanitisedT[F, Either[A, B]]): SanitisedT[F, B] = {
+        ???
       }
     }
 
