664429266607d382a2fe785cc6104891fc0f5a74
Charlie Shaw <charliecpshaw@gmail.com>
2026-07-22 09:30:48 +0100

    feat (tidy): tidy nested types in option/either/ior

--- src/main/scala/macros/tidy.scala
+++ src/main/scala/macros/tidy.scala
@@ -1,40 +1,53 @@
 package experiments.macros
 
-import experiments.macros.hcollections.hchain.{HChain, HSingleton, HAppend, HEmpty, toList}
-import scala.quoted.{Expr, Quotes, Type, quotes}
+import experiments.macros.hcollections.hchain.{HChain, HSingleton, HAppend, HEmpty}
+import scala.quoted.{Expr, Quotes, Type, Varargs, quotes}
 import scala.annotation.tailrec
+import cats.data.Ior
 
 object tidy {
 
   inline def printTidyType[A <: HChain]: Unit = ${ printTidyTypeCode[A] }
 
-  def printTidyTypeCode[A <: HChain: Type](using Quotes): Expr[Unit] = {
+  private def printTidyTypeCode[A <: HChain: Type](using Quotes): Expr[Unit] = {
     import quotes.reflect.TypeRepr
-    val tidied = Expr(TypeRepr.of(using tidyType[A]).show)
-    '{ println($tidied) }
+    
+    val tpe = buildTidyType[A]
+    val str = TypeRepr.of(using tpe).show
+    val strExpr = Expr(str)
+
+    '{ println($strExpr) }
   }
 
-  transparent inline def tidy[A <: HChain](xs: A): Tuple = ${ tidyCode('xs) }
-
-  private def tidyCode[A <: HChain: Type](xs: Expr[A])(using Quotes): Expr[Tuple] = {
-    tidyType[A] match {
-      case '[type a <: Tuple; a] => '{ Tuple.fromArray($xs.toList.toArray).asInstanceOf[a] }
-    }
-  }
-
-  private def tidyType[A <: HChain: Type](using Quotes): Type[?] = {
+  private def buildTidyType[A <: HChain: Type](using Quotes): Type[?] = {
     @tailrec
-    def build[Acc <: Tuple: Type](types: List[Type[? <: HChain]]): Type[? <: Tuple] = types match {
+    def go[Acc <: Tuple: Type](types: List[Type[? <: HChain]]): Type[? <: Tuple] = types match {
       case Nil => Type.of[Acc]
       case tpe :: types => tpe match {
-        case '[HEmpty]        => build[Acc](types)
-        case '[HSingleton[a]] => build[a *: Acc](types)
-        case '[type a <: HChain; type b <: HChain; HAppend[a, b]] => {
-          build[Acc](Type.of[b] :: Type.of[a] :: types)
+        case '[HEmpty] => go[Acc](types)
+        case '[type a <: HChain; HSingleton[Option[a]]] => buildTidyType[a] match {
+          case '[b] => go[Option[b] *: Acc](types)
         }
+        case '[type a <: HChain; type b <: HChain; HSingleton[Either[a, b]]] => buildTidyType[a] match {
+          case '[c] => buildTidyType[b] match {
+            case '[d] => go[Either[c, d] *: Acc](types)
+          }
+        }
+        case '[type a <: HChain; type b <: HChain; HSingleton[Ior[a, b]]] => buildTidyType[a] match {
+          case '[c] => buildTidyType[b] match {
+            case '[d] => go[Ior[c, d] *: Acc](types)
+          }
+        }
+        case '[HSingleton[a]] => go[a *: Acc](types)
+        case '[type a <: HChain; type b <: HChain; HAppend[a, b]] => go[Acc](Type.of[b] :: Type.of[a] :: types)
       }
     }
 
-    build[EmptyTuple](Type.of[A] :: Nil)
+    val tpe = go[EmptyTuple](Type.of[A] :: Nil)
+    tpe match {
+      case '[EmptyTuple] => Type.of[Unit]
+      case '[Tuple1[a]]  => Type.of[a]
+      case _             => tpe
+    }
   }
 }
