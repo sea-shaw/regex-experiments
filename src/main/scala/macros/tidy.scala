04bf608ffc05b1f08dd1469b720cd5cea8c71b11
Charlie Shaw <charliecpshaw@gmail.com>
2026-07-24 12:14:56 +0100

    refactor (tidy): use shared typed expr class

--- src/main/scala/macros/tidy.scala
+++ src/main/scala/macros/tidy.scala
@@ -6,42 +6,43 @@ import scala.quoted.{Expr, Quotes, Type, quotes}
 object tidy {
   class TidyFunction[A <: HChain, B](val tidy: Expr[A] => Quotes ?=> Expr[B], val tpe: Type[B])
 
-  def tidyFunction[A <: HChain: Type](using Quotes): TidyFunction[A, ?] = ???
+  def tidyFunction[A <: HChain: Type](using Quotes): TidyFunction[A, ?] = {
+    ???
+  }
 
   transparent inline def tidy[A <: HChain](xs: A) = ${ tidyCode('xs) }
 
-  private def tidyCode[A <: HChain: Type](xs: Expr[A])(using Quotes): Expr[?] = {
+  def tidyCode[A <: HChain: Type](xs: Expr[A])(using Quotes): Expr[?] = {
 
-    case class TypedNode[A <: HChain](node: Expr[A], tpe: Type[A])
-    case class TypedLeaf[A](leaf: Expr[A], tpe: Type[A])
+    case class TypedExpr[A](expr: Expr[A], tpe: Type[A])
 
-    def go(nodes: List[TypedNode[?]], leaves: List[TypedLeaf[?]])(using Quotes): Expr[?] = nodes match {
+    def go(nodes: List[TypedExpr[?]], leaves: List[TypedExpr[?]])(using Quotes): Expr[?] = nodes match {
       case Nil     => build(leaves)
-      case TypedNode(node, tpe) :: ns => tpe match {
-        case '[HEmpty] => go(ns, leaves)
+      case TypedExpr(node, tpe) :: nodes => tpe match {
+        case '[HEmpty] => go(nodes, leaves)
         case '[HSingleton[a]] => {
           val singleton = node.asExprOf[HSingleton[a]]
-          go(ns, TypedLeaf('{ $singleton.value }, Type.of[a]) :: leaves)
+          go(nodes, TypedExpr('{ $singleton.value }, Type.of[a]) :: leaves)
         }
         case '[type a <: HChain; type b <: HChain; HAppend[a, b]] => {
           val append = node.asExprOf[HAppend[a, b]]
           '{
             val left = $append.left
             val right = $append.right
-            ${ go(TypedNode('{ right }, Type.of[b]) :: TypedNode('{ left }, Type.of[a]) :: ns, leaves) }
+            ${ go(TypedExpr('{ right }, Type.of[b]) :: TypedExpr('{ left }, Type.of[a]) :: nodes, leaves) }
           }
         }
       }
     }
 
-    def build(leaves: List[TypedLeaf[?]])(using Quotes): Expr[?] = {
+    def build(leaves: List[TypedExpr[?]])(using Quotes): Expr[?] = {
       import quotes.reflect.{Position, report}
       val elems = leaves.toVector
 
       elems match {
         case Vector() => '{ () }
-        case Vector(TypedLeaf(e0, _)) => e0
-        case Vector(TypedLeaf(e0, t0), TypedLeaf(e1, t1)) => {
+        case Vector(TypedExpr(e0, _)) => e0
+        case Vector(TypedExpr(e0, t0), TypedExpr(e1, t1)) => {
           type T0 = t0.Underlying
           type T1 = t1.Underlying
 
@@ -50,7 +51,7 @@ object tidy {
 
           '{ ($e0, $e1) }
         }
-        case Vector(TypedLeaf(e0, t0), TypedLeaf(e1, t1), TypedLeaf(e2, t2)) => {
+        case Vector(TypedExpr(e0, t0), TypedExpr(e1, t1), TypedExpr(e2, t2)) => {
           type T0 = t0.Underlying
           type T1 = t1.Underlying
           type T2 = t2.Underlying
@@ -61,7 +62,7 @@ object tidy {
 
           '{ ($e0, $e1, $e2) }
         }
-        case Vector(TypedLeaf(e0, t0), TypedLeaf(e1, t1), TypedLeaf(e2, t2), TypedLeaf(e3, t3)) => {
+        case Vector(TypedExpr(e0, t0), TypedExpr(e1, t1), TypedExpr(e2, t2), TypedExpr(e3, t3)) => {
           type T0 = t0.Underlying
           type T1 = t1.Underlying
           type T2 = t2.Underlying
@@ -78,7 +79,7 @@ object tidy {
       }
     }
 
-    go(TypedNode(xs, Type.of[A]) :: Nil, Nil)
+    go(TypedExpr(xs, Type.of[A]) :: Nil, Nil)
   }
 }
 
