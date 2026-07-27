2bb07054f03eda5a4e1aa4d56389a1105d5fb62f
Charlie Shaw <charliecpshaw@gmail.com>
2026-07-27 10:55:30 +0100

    feat (tidy): implement option, either, and ior tidying

--- src/main/scala/macros/tidy.scala
+++ src/main/scala/macros/tidy.scala
@@ -1,8 +1,10 @@
 package experiments.macros
 
+import cats.data.Ior
+import cats.syntax.all.*
 import experiments.macros.hcollections.hchain.{HChain, HSingleton, HAppend, HEmpty, HNonEmpty}
 import scala.annotation.unused
-import scala.quoted.{Expr, Quotes, Type, quotes}
+import scala.quoted.{Expr, Quotes, Type}
 
 object tidy {
 
@@ -16,9 +18,16 @@ object tidy {
   private case object LNil extends Leaves
   private case class LCons[A, L <: Leaves](head: Expr[A], tail: L) extends Leaves
 
-  class TidyFunction[A <: HChain, B](val tidy: Expr[A] => Quotes ?=> Expr[B], val tpe: Type[B])
+  case class TidyFunction[A <: HChain, B](tidy: Expr[A] => Quotes ?=> Expr[B], tpe: Type[B])
 
-  def tidyFunction2[A <: HChain: Type](using Quotes): TidyFunction[A, ?] = {
+  transparent inline def tidy[A <: HChain](xs: A) = ${ tidyCode('xs) }
+
+  private def tidyCode[A <: HChain: Type](xs: Expr[A])(using Quotes): Expr[?] = {
+    val tidy = tidyFunction[A]
+    tidy.tidy(xs)
+  }
+
+  def tidyFunction[A <: HChain: Type](using Quotes): TidyFunction[A, ?] = {
 
     case class Tidy[-N <: Nodes, -L <: Leaves, B](tidy: (N, L) => Quotes ?=> Expr[B], tpe: Type[B])
     case class Build[-L <: Leaves, B](build: L => Quotes ?=> Expr[B], tpe: Type[B])
@@ -33,6 +42,62 @@ object tidy {
         Tidy(tidy, build.tpe)
       }
       case '[NCons[n, ns]] => Type.of[n] match {
+        case '[type b <: HChain; HSingleton[Option[b]]] => {
+          val inner: TidyFunction[b, ?] = tidyFunction[b]
+          type B = inner.tpe.Underlying
+          given Type[B] = inner.tpe
+
+          val next = go[ns, LCons[Option[B], L]]
+          type C = next.tpe.Underlying
+
+          def tidy(nodes: N, leaves: L)(using Quotes): Expr[C] = {
+            // TODO
+            val NCons(node, tail) = nodes.asInstanceOf[NCons[HSingleton[Option[b]], ns]]
+            next.tidy(tail, LCons('{ $node.value.map(x => ${ inner.tidy('x) }) }, leaves))
+          }
+
+          Tidy(tidy, next.tpe)
+        }
+        case '[type b <: HChain; type c <: HChain; HSingleton[Either[b, c]]] => {
+          val left: TidyFunction[b, ?] = tidyFunction[b]
+          type B = left.tpe.Underlying
+          given Type[B] = left.tpe
+
+          val right: TidyFunction[c, ?] = tidyFunction[c]
+          type C = right.tpe.Underlying
+          given Type[C] = right.tpe
+
+          val next = go[ns, LCons[Either[B, C], L]]
+          type D = next.tpe.Underlying
+
+          def tidy(nodes: N, leaves: L)(using Quotes): Expr[D] = {
+            // TODO
+            val NCons(node, tail) = nodes.asInstanceOf[NCons[HSingleton[Either[b, c]], ns]]
+            next.tidy(tail, LCons('{ $node.value.bimap(x => ${ left.tidy('x) }, x => ${ right.tidy('x) }) }, leaves))
+          }
+
+          Tidy(tidy, next.tpe)
+        }
+        case '[type b <: HChain; type c <: HChain; HSingleton[Ior[b, c]]] => {
+          val left: TidyFunction[b, ?] = tidyFunction[b]
+          type B = left.tpe.Underlying
+          given Type[B] = left.tpe
+
+          val right: TidyFunction[c, ?] = tidyFunction[c]
+          type C = right.tpe.Underlying
+          given Type[C] = right.tpe
+
+          val next = go[ns, LCons[Ior[B, C], L]]
+          type D = next.tpe.Underlying
+
+          def tidy(nodes: N, leaves: L)(using Quotes): Expr[D] = {
+            // TODO
+            val NCons(node, tail) = nodes.asInstanceOf[NCons[HSingleton[Ior[b, c]], ns]]
+            next.tidy(tail, LCons('{ $node.value.bimap(x => ${ left.tidy('x) }, x => ${ right.tidy('x) }) }, leaves))
+          }
+
+          Tidy(tidy, next.tpe)
+        }
         case '[HSingleton[a]] => {
           val next = go[ns, LCons[a, L]]
           type B = next.tpe.Underlying
@@ -97,141 +162,12 @@ object tidy {
       }
     }
 
-    // val tidyHList: Tidy[NCons[A, NNil], LNil, ?] = go[NCons[A, NNil], LNil]
-    // type B = tidyHList.tpe.Underlying
-    // TidyFunction[A, B](xs => tidyHList.tidy(NCons(xs, NNil), LNil), tidyHList.tpe)
-    ???
-  }
-
-  def tidyFunction[A <: HChain: Type](using Quotes): TidyFunction[A, ?] = {
-    val typed: Typed[?] = tidyType[A]
-    type B = typed.tpe.Underlying
-    given Type[B] = typed.tpe
-
-    def tidy(xs: Expr[A])(using Quotes): Expr[B] = tidyCode[A](xs).asExprOf[B]
-
-    TidyFunction(tidy, typed.tpe)
-  }
-
-  transparent inline def tidy[A <: HChain](xs: A) = ${ tidyCode('xs) }
-
-  def tidyCode[A <: HChain: Type](xs: Expr[A])(using Quotes): Expr[?] = {
-
-    case class TypedExpr[A](expr: Expr[A], tpe: Type[A])
-
-    def go(nodes: List[TypedExpr[? <: HChain]], leaves: List[TypedExpr[?]])(using Quotes): Expr[?] = nodes match {
-      case Nil     => build(leaves)
-      case TypedExpr(node, tpe) :: nodes => tpe match {
-        case '[HEmpty] => go(nodes, leaves)
-        case '[HSingleton[a]] => {
-          val singleton = node.asExprOf[HSingleton[a]]
-          go(nodes, TypedExpr('{ $singleton.value }, Type.of[a]) :: leaves)
-        }
-        case '[type a <: HChain; type b <: HChain; HAppend[a, b]] => {
-          val append = node.asExprOf[HAppend[a, b]]
-          '{
-            val left = $append.left
-            val right = $append.right
-            ${ go(TypedExpr('{ right }, Type.of[b]) :: TypedExpr('{ left }, Type.of[a]) :: nodes, leaves) }
-          }
-        }
+    Type.of[A] match {
+      case '[HEmpty] => TidyFunction[A, Unit](_ => _ ?=> '{ () }, Type.of[Unit])
+      case '[type a <: HNonEmpty; a] => {
+        val tidyFunction = go[NCons[a, NNil], LNil]
+        TidyFunction(xs => _ ?=> tidyFunction.tidy(NCons(xs.asExprOf[a], NNil), LNil), tidyFunction.tpe)
       }
     }
-
-    def build(leaves: List[TypedExpr[?]])(using Quotes): Expr[?] = {
-      import quotes.reflect.{Position, report}
-      val elems = leaves.toVector
-
-      elems match {
-        case Vector() => '{ () }
-        case Vector(TypedExpr(e0, _)) => e0
-        case Vector(TypedExpr(e0, t0), TypedExpr(e1, t1)) => {
-          type T0 = t0.Underlying
-          type T1 = t1.Underlying
-
-          given Type[T0] = t0
-          given Type[T1] = t1
-
-          '{ ($e0, $e1) }
-        }
-        case Vector(TypedExpr(e0, t0), TypedExpr(e1, t1), TypedExpr(e2, t2)) => {
-          type T0 = t0.Underlying
-          type T1 = t1.Underlying
-          type T2 = t2.Underlying
-
-          given Type[T0] = t0
-          given Type[T1] = t1
-          given Type[T2] = t2
-
-          '{ ($e0, $e1, $e2) }
-        }
-        case Vector(TypedExpr(e0, t0), TypedExpr(e1, t1), TypedExpr(e2, t2), TypedExpr(e3, t3)) => {
-          type T0 = t0.Underlying
-          type T1 = t1.Underlying
-          type T2 = t2.Underlying
-          type T3 = t3.Underlying
-
-          given Type[T0] = t0
-          given Type[T1] = t1
-          given Type[T2] = t2
-          given Type[T3] = t3
-
-          '{ ($e0, $e1, $e2, $e3) }
-        }
-        case _ => report.errorAndAbort(s"Unsupported tuple size ${elems.size}", Position.ofMacroExpansion)
-      }
-    }
-
-    go(TypedExpr(xs, Type.of[A]) :: Nil, Nil)
-  }
-
-  case class Typed[A: Type](tpe: Type[A])
-  object Typed {
-    def of[A: Type](using Quotes) = Typed(Type.of[A])
-  }
-
-  def tidyType[A <: HChain: Type](using Quotes): Typed[?] = {
-
-    def go(nodes: List[Typed[? <: HChain]], leaves: List[Typed[?]])(using Quotes): Typed[?] = nodes match {
-      case Nil => build(leaves)
-      case node :: nodes => node.tpe match {
-        case '[HEmpty] => go(nodes, leaves)
-        case '[HSingleton[a]] => go(nodes, Typed.of[a] :: leaves)
-        case '[type a <: HChain; type b <: HChain; HAppend[a, b]] => {
-          go(Typed.of[b] :: Typed.of[a] :: nodes, leaves)
-        }
-      }
-    }
-
-    def build(leaves: List[Typed[?]])(using Quotes): Typed[?] = {
-      import quotes.reflect.{Position, report}
-
-      val types = leaves.toVector
-      types match {
-        case Vector() => Typed.of[Unit]
-        case Vector(t0) => t0
-        case Vector(Typed(t0), Typed(t1)) => {
-          type T0 = t0.Underlying
-          type T1 = t1.Underlying
-          Typed.of[(T0, T1)]
-        }
-        case Vector(Typed(t0), Typed(t1), Typed(t2)) => {
-          type T0 = t0.Underlying
-          type T1 = t1.Underlying
-          type T2 = t2.Underlying
-          Typed.of[(T0, T1, T2)]
-        }
-        case Vector(Typed(t0), Typed(t1), Typed(t2), Typed(t3)) => {
-          type T0 = t0.Underlying
-          type T1 = t1.Underlying
-          type T2 = t2.Underlying
-          type T3 = t3.Underlying
-          Typed.of[(T0, T1, T2, T3)]
-        }
-        case _ => report.errorAndAbort(s"Unsupported tuple size ${types.size}", Position.ofMacroExpansion)
-      }
-    }
-
-    go(Typed(Type.of[A]) :: Nil, Nil)
   }
 }
