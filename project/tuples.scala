49cc92f876f3d928efe1dc9be362df4084a361f1
Charlie Shaw <charliecpshaw@gmail.com>
2026-08-18 16:07:56 +0100

    refactor (tuples): remove magic numbers

--- project/tuples.scala
+++ project/tuples.scala
@@ -2,15 +2,11 @@ import sbt.*
 
 object tuples {
 
-  def gen(dir: File): Seq[File] = {
-    val file = filename(dir)
-    IO.write(file, content)
-    Seq(file)
-  }
+  private val minSize = 2
+  private val maxSize = 22
+  private val indentSize = 2
 
-  def filename(root: File): File = root / "scala"/ "macros" / "tidy.scala"
-
-  val content = s"""|package experiments.macros
+  private val content = s"""|package experiments.macros
     |
     |import experiments.macros.ast.AST
     |import scala.quoted.{Expr, Quotes, Type, quotes}
@@ -23,34 +19,42 @@ object tuples {
     |        case TNil => new BuildFunction[ToLeaves, t0] {
     |          override def apply(leaves: ToLeaves)(using Quotes): Expr[t0] = leaves.head
     |        }
-    |${ tupleCase(2).indent(8).stripLineEnd }
+    |${ tconsCase(minSize).indent(4 * indentSize).stripLineEnd }
     |      }
     |    }
     |  }
     |}
     |""".stripMargin
 
-    def tupleCase(n: Int): String = {
-      if (n <= 22) {
-        val indices = (0 until n)
-        val tupleType = indices.reverse.map(i => s"t$i").mkString("(", ", ", ")")
-        val tupleExpr = indices.reverse.map(i => s"$$e$i").mkString("'{ (", ", ", ") }")
-        val leavesPattern = indices.map(i => s"LCons(e$i, ").mkString("", "", s"LNil${")" * n}")
-        val leavesType = indices.map(i => s"LCons[t$i, ").mkString("", "", s"LNil${"]" * n}")
-        s"""|case TCons(given Type[t${n - 1}], tail${n - 1}) => tail${n - 1} match {
-            |  case TNil => new BuildFunction[ToLeaves, $tupleType] {
-            |    override def apply(leaves: ToLeaves)(using Quotes): Expr[$tupleType] = {
-            |      val $leavesPattern = leaves.asInstanceOf[$leavesType]
-            |      $tupleExpr
-            |    }
-            |  }
-            |${ tupleCase(n + 1).indent(2).stripLineEnd }
-            |}""".stripMargin
-      } else {
-        """|case _ => {
-           |  import quotes.reflect.{Position, report}
-           |  report.errorAndAbort("Tuples of size >22 are not supported.", Position.ofMacroExpansion)
-           |}""".stripMargin
-      }
+  def gen(dir: File): Seq[File] = {
+    val file = filename(dir)
+    IO.write(file, content)
+    Seq(file)
+  }
+
+  private def filename(root: File): File = root / "scala"/ "macros" / "tidy.scala"
+
+  private def tconsCase(n: Int): String = {
+    if (n <= maxSize) {
+      val indices = (0 until n)
+      val tupleType = indices.reverse.map(i => s"t$i").mkString("(", ", ", ")")
+      val tupleExpr = indices.reverse.map(i => s"$$e$i").mkString("'{ (", ", ", ") }")
+      val leavesPattern = indices.map(i => s"LCons(e$i, ").mkString("", "", s"LNil${")" * n}")
+      val leavesType = indices.map(i => s"LCons[t$i, ").mkString("", "", s"LNil${"]" * n}")
+      s"""|case TCons(given Type[t${n - 1}], tail${n - 1}) => tail${n - 1} match {
+          |  case TNil => new BuildFunction[ToLeaves, $tupleType] {
+          |    override def apply(leaves: ToLeaves)(using Quotes): Expr[$tupleType] = {
+          |      val $leavesPattern = leaves.asInstanceOf[$leavesType]
+          |      $tupleExpr
+          |    }
+          |  }
+          |${ tconsCase(n + 1).indent(indentSize).stripLineEnd }
+          |}""".stripMargin
+    } else {
+      s"""|case _ => {
+          |  import quotes.reflect.{Position, report}
+          |  report.errorAndAbort("Tuples of size >$maxSize are not supported.", Position.ofMacroExpansion)
+          |}""".stripMargin
     }
+  }
 }
