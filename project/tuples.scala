79156debb79c2c835734125113b9b07650dd6c81
Charlie Shaw <charliecpshaw@gmail.com>
2026-08-18 14:49:54 +0100

    refactor (tidy): use lowercase types for consistency

--- project/tuples.scala
+++ project/tuples.scala
@@ -17,30 +17,30 @@ object tuples {
     |
     |object tidy {
     |  trait Tidy extends AST {
-    |    override protected def tconsBuildFunction[T0: Type](tail: Types)(using Quotes): BuildFunction[LCons[T0, tail.ToLeaves], ?] = {
-    |      type ToLeaves = LCons[T0, tail.ToLeaves]
+    |    override protected def tconsBuildFunction[t0: Type](tail: Types)(using Quotes): BuildFunction[LCons[t0, tail.ToLeaves], ?] = {
+    |      type ToLeaves = LCons[t0, tail.ToLeaves]
     |      tail match {
-    |        case TNil => new BuildFunction[ToLeaves, T0] {
-    |          override def apply(leaves: ToLeaves)(using Quotes): Expr[T0] = leaves.head
+    |        case TNil => new BuildFunction[ToLeaves, t0] {
+    |          override def apply(leaves: ToLeaves)(using Quotes): Expr[t0] = leaves.head
     |        }
     |        case TCons(given Type[t1], tail1) => tail1 match {
-    |          case TNil => new BuildFunction[ToLeaves, (t1, T0)] {
-    |            override def apply(leaves: ToLeaves)(using Quotes): Expr[(t1, T0)] = {
-    |              val LCons(e0, LCons(e1, LNil)) = leaves.asInstanceOf[LCons[T0, LCons[t1, LNil]]]
+    |          case TNil => new BuildFunction[ToLeaves, (t1, t0)] {
+    |            override def apply(leaves: ToLeaves)(using Quotes): Expr[(t1, t0)] = {
+    |              val LCons(e0, LCons(e1, LNil)) = leaves.asInstanceOf[LCons[t0, LCons[t1, LNil]]]
     |              '{ ($e1, $e0) }
     |            }
     |          }
     |          case TCons(given Type[t2], tail2) => tail2 match {
-    |            case TNil => new BuildFunction[ToLeaves, (t2, t1, T0)] {
-    |              override def apply(leaves: ToLeaves)(using Quotes): Expr[(t2, t1, T0)] = {
-    |                val LCons(e0, LCons(e1, LCons(e2, LNil))) = leaves.asInstanceOf[LCons[T0, LCons[t1, LCons[t2, LNil]]]]
+    |            case TNil => new BuildFunction[ToLeaves, (t2, t1, t0)] {
+    |              override def apply(leaves: ToLeaves)(using Quotes): Expr[(t2, t1, t0)] = {
+    |                val LCons(e0, LCons(e1, LCons(e2, LNil))) = leaves.asInstanceOf[LCons[t0, LCons[t1, LCons[t2, LNil]]]]
     |                '{ ($e2, $e1, $e0) }
     |              }
     |            }
     |            case TCons(given Type[t3], tail3) => tail3 match {
-    |              case TNil => new BuildFunction[ToLeaves, (t3, t2, t1, T0)] {
-    |                override def apply(leaves: ToLeaves)(using Quotes): Expr[(t3, t2, t1, T0)] = {
-    |                  val LCons(e0, LCons(e1, LCons(e2, LCons(e3, LNil)))) = leaves.asInstanceOf[LCons[T0, LCons[t1, LCons[t2, LCons[t3, LNil]]]]]
+    |              case TNil => new BuildFunction[ToLeaves, (t3, t2, t1, t0)] {
+    |                override def apply(leaves: ToLeaves)(using Quotes): Expr[(t3, t2, t1, t0)] = {
+    |                  val LCons(e0, LCons(e1, LCons(e2, LCons(e3, LNil)))) = leaves.asInstanceOf[LCons[t0, LCons[t1, LCons[t2, LCons[t3, LNil]]]]]
     |                  '{ ($e3, $e2, $e1, $e0) }
     |                }
     |              }
@@ -53,4 +53,26 @@ object tuples {
     |  }
     |}
     |""".stripMargin
+
+    def tupleCase(n: Int): String = {
+      if (n <= 22) {
+        val indices = (0 until n)
+        val tupleType = indices.reverse.map(i => s"t$i").mkString("(", ", ", ")")
+        val tupleExpr = indices.reverse.map(i => s"$$e$i").mkString("'{ (", ", ", ") }")
+        val leavesPattern = indices.map(i => s"LCons(e$i, ").mkString("", "", s"LNil${")" * n}")
+        val leavesType = indices.map(i => s"LCons[t$i, ").mkString("", "", s"LNil${"]" * n}")
+        s"""|case TCons(given Type[t$n], tail$n) => tail$n match {
+            |  case TNil => new BuildFunction[ToLeaves, $tupleType] {
+            |    override def apply(leaves: ToLeaves)(using Quotes): Expr[$tupleType] = {
+            |      val $leavesPattern = leaves.asInstanceOf[$leavesType]
+            |      $tupleExpr
+            |    }
+            |  }
+            |${tupleCase(n + 1).indent(2)}
+            |}
+        """.stripMargin
+      } else {
+        "case _ => ???"
+      }
+    }
 }
--- src/main/scala/macros/ast.scala
+++ src/main/scala/macros/ast.scala
@@ -155,7 +155,7 @@ object ast {
     protected def inclusiveOrType(using Quotes): Type[InclusiveOr]
     protected def fromOptions[A: Type, B: Type](left: Expr[Option[A]], right: Expr[Option[B]])(using Quotes): Expr[Option[InclusiveOr[A, B]]]
     protected def bimap[A: Type, B: Type, C: Type, D: Type](f: Expr[A] => Quotes ?=> Expr[C], g: Expr[B] => Quotes ?=> Expr[D])(expr: Expr[InclusiveOr[A, B]])(using Quotes): Expr[InclusiveOr[C, D]]
-    protected def tconsBuildFunction[T0: Type](tail: Types)(using Quotes): BuildFunction[LCons[T0, tail.ToLeaves], ?]
+    protected def tconsBuildFunction[A: Type](tail: Types)(using Quotes): BuildFunction[LCons[A, tail.ToLeaves], ?]
 
     private sealed trait Nodes {
       type ToChains <: Chains
@@ -203,10 +203,10 @@ object ast {
       }
     }
 
-    protected case class TCons[T0, T <: Types & Singleton](head: Type[T0], tail: T) extends Types {
-      type ToLeaves = LCons[T0, tail.ToLeaves]
+    protected case class TCons[A, T <: Types & Singleton](head: Type[A], tail: T) extends Types {
+      type ToLeaves = LCons[A, tail.ToLeaves]
       override def buildFunction(using Quotes): BuildFunction[ToLeaves, ?] = {
-        given Type[T0] = head
+        given Type[A] = head
         tconsBuildFunction(tail)
       }
     }
