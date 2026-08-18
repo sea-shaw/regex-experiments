5fdec65c57995d44abbe33a68899b4f816185fd8
Charlie Shaw <charliecpshaw@gmail.com>
2026-08-18 15:03:42 +0100

    feat (tuples): implement source generator up to size 22

--- project/tuples.scala
+++ project/tuples.scala
@@ -10,7 +10,7 @@ object tuples {
 
   def filename(root: File): File = root / "scala"/ "macros" / "tidy.scala"
 
-  val content = """|package experiments.macros
+  val content = s"""|package experiments.macros
     |
     |import experiments.macros.ast.AST
     |import scala.quoted.{Expr, Quotes, Type}
@@ -23,31 +23,7 @@ object tuples {
     |        case TNil => new BuildFunction[ToLeaves, t0] {
     |          override def apply(leaves: ToLeaves)(using Quotes): Expr[t0] = leaves.head
     |        }
-    |        case TCons(given Type[t1], tail1) => tail1 match {
-    |          case TNil => new BuildFunction[ToLeaves, (t1, t0)] {
-    |            override def apply(leaves: ToLeaves)(using Quotes): Expr[(t1, t0)] = {
-    |              val LCons(e0, LCons(e1, LNil)) = leaves.asInstanceOf[LCons[t0, LCons[t1, LNil]]]
-    |              '{ ($e1, $e0) }
-    |            }
-    |          }
-    |          case TCons(given Type[t2], tail2) => tail2 match {
-    |            case TNil => new BuildFunction[ToLeaves, (t2, t1, t0)] {
-    |              override def apply(leaves: ToLeaves)(using Quotes): Expr[(t2, t1, t0)] = {
-    |                val LCons(e0, LCons(e1, LCons(e2, LNil))) = leaves.asInstanceOf[LCons[t0, LCons[t1, LCons[t2, LNil]]]]
-    |                '{ ($e2, $e1, $e0) }
-    |              }
-    |            }
-    |            case TCons(given Type[t3], tail3) => tail3 match {
-    |              case TNil => new BuildFunction[ToLeaves, (t3, t2, t1, t0)] {
-    |                override def apply(leaves: ToLeaves)(using Quotes): Expr[(t3, t2, t1, t0)] = {
-    |                  val LCons(e0, LCons(e1, LCons(e2, LCons(e3, LNil)))) = leaves.asInstanceOf[LCons[t0, LCons[t1, LCons[t2, LCons[t3, LNil]]]]]
-    |                  '{ ($e3, $e2, $e1, $e0) }
-    |                }
-    |              }
-    |              case TCons(_, _) => ???
-    |            }
-    |          }
-    |        }
+    |${ tupleCase(2).indent(8).stripLineEnd }
     |      }
     |    }
     |  }
@@ -61,16 +37,15 @@ object tuples {
         val tupleExpr = indices.reverse.map(i => s"$$e$i").mkString("'{ (", ", ", ") }")
         val leavesPattern = indices.map(i => s"LCons(e$i, ").mkString("", "", s"LNil${")" * n}")
         val leavesType = indices.map(i => s"LCons[t$i, ").mkString("", "", s"LNil${"]" * n}")
-        s"""|case TCons(given Type[t$n], tail$n) => tail$n match {
+        s"""|case TCons(given Type[t${n - 1}], tail${n - 1}) => tail${n - 1} match {
             |  case TNil => new BuildFunction[ToLeaves, $tupleType] {
             |    override def apply(leaves: ToLeaves)(using Quotes): Expr[$tupleType] = {
             |      val $leavesPattern = leaves.asInstanceOf[$leavesType]
             |      $tupleExpr
             |    }
             |  }
-            |${tupleCase(n + 1).indent(2)}
-            |}
-        """.stripMargin
+            |${tupleCase(n + 1).indent(2).stripLineEnd}
+            |}""".stripMargin
       } else {
         "case _ => ???"
       }
