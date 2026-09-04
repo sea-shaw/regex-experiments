2ad087525325efd11827422886fff50b83ad0900
Charlie Shaw <charliecpshaw@gmail.com>
2026-09-04 12:05:19 +0100

    refactor (ast): move opt sanitise and flatten functions to node types

--- macros/src/main/scala/ast.scala
+++ macros/src/main/scala/ast.scala
@@ -785,20 +785,59 @@ object ast {
       }
     }
 
-    sealed trait OptType[F[_ <: Rep] <: HChain, G[_ <: Rep] <: HChain] extends NodeType[G]
+    sealed trait OptType[F[_ <: Rep] <: HChain, G[_ <: Rep] <: HChain] extends NodeType[G] {
+      def sanitiseCode[R <: Rep: Type](sanitisedInner: => SanitiseExpr[F[R]])(using RepType[R])(using Quotes): SanitiseExpr[G[R]]
+      private [AST] def flattenFunction[C <: Chains, L <: Leaves, R <: Rep: Type](nodes: Nodes[C], types: Types[L])(using RepType[R])(using Quotes): FlattenFunction[CCons[G[R], C], L, ?]
+    }
 
     /* A? */
-    private case class OptEmpty()(using Type[Const[HEmpty]]) extends OptType[Const[HEmpty], Const[HEmpty]] with HEmptyType
+    private case class OptEmpty()(using Type[Const[HEmpty]]) extends OptType[Const[HEmpty], Const[HEmpty]] with HEmptyType {
+      override def sanitiseCode[R <: Rep: Type](sanitisedInner: => SanitiseExpr[Const[HEmpty][R]])(using RepType[R])(using Quotes): SanitiseExpr[Const[HEmpty][R]] = {
+        sanitiseEmpty
+      }
+
+      override def flattenFunction[C <: Chains, L <: Leaves, R <: Rep: Type](nodes: Nodes[C], types: Types[L])(using RepType[R])(using Quotes): FlattenFunction[CCons[HEmpty, C], L, ?] = {
+        flattenEmpty(nodes, types)
+      }
+    }
 
     /* (A)? */
     private type OptSingletonType = SingletonOptionType
     private case class OptSingleton[F[_ <: Rep] <: HNonEmpty]()(inner: Regex[F])(using Type[F], Type[OptSingletonType[F]]) extends OptType[F, OptSingletonType[F]] with SingletonOption[F] {
+      override def sanitiseCode[R <: Rep: Type](sanitisedInner: => SanitiseExpr[F[R]])(using RepType[R])(using Quotes): SanitiseExpr[OptSingletonType[F][R]] = {
+        '{
+          val innerCaps = $sanitisedInner
+          SanitisedT(Some(innerCaps.value.sequence.map(HSingleton(_))))
+        }
+      }
+
+      override def flattenFunction[C <: Chains, L <: Leaves, R <: Rep: Type](nodes: Nodes[C], types: Types[L])(using RepType[R])(using Quotes): FlattenFunction[CCons[HSingleton[Option[F[R]]], C], L, ?] = {
+        flattenOpt(inner.tidyFunction, nodes, types)
+      }
+
       override def tidyInner[R <: Rep: Type](using RepType[R])(using Quotes): TidyFunction[F[R], ?] = inner.tidyFunction
     }
 
     /* (A?)? */
     private type OptNestedType = SingletonOptionType
     private case class OptNested[F[_ <: Rep] <: HNonEmpty]()(innerType: SingletonOption[F])(using Type[F], Type[OptNestedType[F]]) extends OptType[OptNestedType[F], OptNestedType[F]] with SingletonOption[F] {
+      override def sanitiseCode[R <: Rep: Type](sanitisedInner: => SanitiseExpr[OptNestedType[F][R]])(using RepType[R])(using Quotes): SanitiseExpr[OptNestedType[F][R]] = {
+        sanitisedInner
+      }
+
+      override def flattenFunction[C <: Chains, L <: Leaves, R <: Rep: Type](nodes: Nodes[C], types: Types[L])(using RepType[R])(using Quotes): FlattenFunction[CCons[HSingleton[Option[F[R]]], C], L, ?] = {
+        tidyInner match {
+          case tidy @ TidyFunction(given Type[a]) => nodes.flattenFunction(TCons(Type.of[Option[a]], types)) match {
+            case flatten @ FlattenFunction(given Type[b]) => new FlattenFunction[CCons[HSingleton[Option[F[R]]], C], L, b] {
+              override def apply(chains: CCons[HSingleton[Option[F[R]]], C], leaves: L)(using Quotes): Expr[b] = {
+                val opt = '{ ${ chains.head }.value.map(node => ${ tidy('node) }) }
+                flatten(chains.tail, LCons(opt, leaves))
+              }
+            }
+          }
+        }
+      }
+
       override def tidyInner[R <: Rep: Type](using RepType[R])(using Quotes): TidyFunction[F[R], ?] = innerType.tidyInner
     }
 
@@ -811,24 +850,11 @@ object ast {
       override def sanitiseCode[R <: Rep: Type](groups: Expr[Groups], i: Int)(using RepType[R])(using Quotes): SanitiseExpr[G[R]] = {
         lazy val sanitisedInner = inner.sanitiseCode(groups, i)
 
-        nodeType match {
-          case OptEmpty() => sanitiseEmpty
-          case OptNested() => sanitisedInner
-          case OptSingleton() => {
-            '{
-              val innerCaps = $sanitisedInner
-              SanitisedT(Some(innerCaps.value.sequence.map(HSingleton(_))))
-            }
-          }
-        }
+        nodeType.sanitiseCode(sanitisedInner)
       }
 
       override private [AST] def flattenFunction[C <: Chains, L <: Leaves, R <: Rep: Type](nodes: Nodes[C], types: Types[L])(using RepType[R])(using Quotes): FlattenFunction[CCons[G[R], C], L, ?] = {
-        nodeType match {
-          case OptEmpty()     => flattenEmpty(nodes, types)
-          case OptNested()    => inner.flattenFunction(nodes, types)
-          case OptSingleton() => flattenOpt(inner.tidyFunction, nodes, types)
-        }
+        nodeType.flattenFunction(nodes, types)
       }
     }
 
