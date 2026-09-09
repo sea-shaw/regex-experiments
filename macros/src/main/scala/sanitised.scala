05d36be1ca0af758502448219f299344a9e2ac3d
Charlie Shaw <charliecpshaw@gmail.com>
2026-09-09 14:07:22 +0100

    refactor (sanitised): use opaque type for transformer

--- macros/src/main/scala/sanitised.scala
+++ macros/src/main/scala/sanitised.scala
@@ -66,51 +66,53 @@ object sanitised {
     given [A] => Order[Sanitised[A]] = Order.by(_.any)
   }
 
-  case class SanitisedT[F[_], A](value: F[Sanitised[A]])
+  opaque type SanitisedT[F[_], A] = F[Sanitised[A]]
+
+  extension [F[_], A] (sanitisedT: SanitisedT[F, A]) {
+    def value: F[Sanitised[A]] = sanitisedT
+  }
 
   object SanitisedT {
-    given [F[_]: Functor] => Functor[[A] =>> SanitisedT[F, A]] {
+
+    def apply[F[_], A](value: F[Sanitised[A]]): SanitisedT[F, A] = value
+
+    given [F[_]: Functor as F] => Functor[[A] =>> SanitisedT[F, A]] {
       override def map[A, B](fa: SanitisedT[F, A])(f: A => B): SanitisedT[F, B] = {
-        SanitisedT(fa.value.map(_.map(f)))
+        F.map(fa)(_.map(f))
       }
     }
 
-    given [F[_]: Applicative] => Applicative[[A] =>> SanitisedT[F, A]] {
+    given [F[_]: Applicative as F] => Applicative[[A] =>> SanitisedT[F, A]] {
       override def pure[A](x: A): SanitisedT[F, A] = {
-        SanitisedT(Applicative[F].pure(Applicative[Sanitised].pure(x)))
+        F.pure(Applicative[Sanitised].pure(x))
       }
 
       override def ap[A, B](ff: SanitisedT[F, A => B])(fa: SanitisedT[F, A]): SanitisedT[F, B] = {
-        SanitisedT((ff.value, fa.value).mapN(_ ap _))
+        F.map2(ff, fa)(_ ap _)
       }
     }
 
-    given [F[_]: Monad] => Monad[[A] =>> SanitisedT[F, A]] {
+    given [F[_]: Monad as F] => Monad[[A] =>> SanitisedT[F, A]] {
       override def pure[A](x: A): SanitisedT[F, A] = {
-        SanitisedT(Monad[F].pure(Monad[Sanitised].pure(x)))
+        F.pure(Monad[Sanitised].pure(x))
       }
 
       override def flatMap[A, B](fa: SanitisedT[F, A])(f: A => SanitisedT[F, B]): SanitisedT[F, B] = {
-        val fb = Monad[F].flatMap(fa.value) { a =>
-          f(a.captures).value.map(b => Sanitised(b.captures, a.any || b.any))
+        F.flatMap(fa) { a =>
+          F.map(f(a.captures))(b => Sanitised(b.captures, a.any || b.any))
         }
-        SanitisedT(fb)
       }
 
       override def tailRecM[A, B](a: A)(f: A => SanitisedT[F, Either[A, B]]): SanitisedT[F, B] = {
-        SanitisedT(
-          Monad[F].tailRecM((a, false)) { (x, any) =>
-            f(x).value.map {
-              case Sanitised(Left(left), leftAny) => Left(left, any || leftAny)
-              case Sanitised(Right(right), rightAny) => Right(Sanitised(right, any || rightAny))
-            }
+        F.tailRecM((a, false)) { (x, any) =>
+          F.map(f(x)) {
+            case Sanitised(Left(left), leftAny)    => Left(left, any || leftAny)
+            case Sanitised(Right(right), rightAny) => Right(Sanitised(right, any || rightAny))
           }
-        )
+        }
       }
     }
 
-    given [F[_], A] => Order[F[Sanitised[A]]] => Order[SanitisedT[F, A]] = {
-      Order.by(_.value)
-    }
+    given [F[_], A] => (order: Order[F[Sanitised[A]]]) => Order[SanitisedT[F, A]] = order
   }
 }
